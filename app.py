7c9433d42be4336eeba982ab0193ad77615c3321
luizfonseca <luiz.fonseca@gerdau.com>
2023-11-06 15:04:18 -0500

    second commit: PDF chat POC

---
+++ app.py
@@ -0,0 +1,30 @@
+import tempfile
+from PIL import Image
+
+# Import os to set API key
+import os
+# Import OpenAI as main LLM service
+from langchain.llms import OpenAI
+from langchain.embeddings import OpenAIEmbeddings
+# Bring in streamlit for UI/app interface
+import streamlit as st
+
+# Import PDF document loaders...there's other ones as well!
+from langchain.document_loaders import PyPDFLoader
+# Import chroma as the vector store
+from langchain.vectorstores import Chroma
+
+# Import vector store stuff
+from langchain.agents.agent_toolkits import (
+    create_vectorstore_agent,
+    VectorStoreToolkit,
+    VectorStoreInfo
+)
+
+# Set the title and subtitle of the app
+st.title('🦜🔗 PDF-Chat: Interact with Your PDFs in a Conversational Way')
+st.subheader('Load your PDF, ask questions, and receive answers directly from the document.')
+
+# Load the image
+# image = Image.open('PDF-Chat App.png')
+# st.image(image)
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from config import OPEN_AI_API_KEY
+
+
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
@@ -11,6 +14,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    print_hi(OPEN_AI_API_KEY)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
